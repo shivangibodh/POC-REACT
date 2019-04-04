b92aa5f031c9b7ca9beec38c5e1bc7a5a7ca2188
shivangibodh <47949043+shivangibodh@users.noreply.github.com>
2019-04-04 19:14:54 +0530

    Create reducers>reducer-users.js

---
+++ reducers>reducer-users.js
@@ -0,0 +1,18 @@
+export default function(){
+
+    return [
+
+        {
+            id:1,
+            name:'shivangi bodh',
+            contact:'522154562',
+            email:'shivangi@gmail.com'
+        },
+        
+          {  id:2,
+            name:'gdhgsdg bodh',
+            contact:'21141562',
+            email:'shivangi@gmail.com'
+        }
+            ]
+}
